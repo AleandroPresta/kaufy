bd8500e5a341333957be9c7c99a9d19ebd5bc116
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-27 14:35:02 +0200

    Scaffolding search

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-search-bar',
-  imports: [],
-  templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+    selector: 'app-search-bar',
+    imports: [],
+    templateUrl: './search-bar.component.html',
+    styleUrl: './search-bar.component.css',
 })
 export class SearchBarComponent {
+    @Output() searchQuery: EventEmitter<string> = new EventEmitter<string>();
 
+    search(): void {
+        const query = (
+            document.getElementById('searchInput') as HTMLInputElement
+        ).value;
+        if (query) {
+            console.log('Search query:', query);
+            this.searchQuery.emit(query);
+            // Clear the input field after emitting the event
+            (document.getElementById('searchInput') as HTMLInputElement).value =
+                '';
+        } else {
+            this.invalidQuery();
+        }
+    }
+
+    invalidQuery(): void {
+        console.log('Invalid search query');
+    }
 }
