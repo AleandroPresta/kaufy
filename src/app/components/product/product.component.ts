031da63f7bd875dd41df9b1ac98c40efe5711bc8
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-29 17:29:11 +0200

    feat: enhance product model and integrate product list in shop list component

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from '../../models/product.model';
 
 @Component({
@@ -9,10 +9,10 @@ import { Product } from '../../models/product.model';
     styleUrl: './product.component.css',
 })
 export class ProductComponent {
-    product: Product = {
-        name: 'Wireless Headphones',
-        price: 199,
-        image_url:
-            'https://lh3.googleusercontent.com/aida-public/AB6AXuBubWdtPnOBX8w8X54hL1hVhBTBKU6oT__9wUH3oCylRHiXDrPpwutrL8-hp02b5zrUmpECGbW09Ok_JR-PIGl1WcT78-Do64OFFrTLx8Zqa2YxM7lY4GKD1aevcJgSvqMNkS-ygsrXcQIxzjroktjzRjCResdDjS-YizKU20825g_QGNmenVTGUCm4jW2cFofmv7R21dH6OxLkVzvi1aE_EJBMBoUDJg4GEtBBTgrNUyMXiVssY57JGLE1t9ymnv4JooeqBS0vFw',
+    @Input() product: Product = {
+        name: 'Product Name',
+        price: 0,
+        image_url: 'https://placehold.co/150x150/png?text=Product+Image',
+        description: 'Product Description',
     };
 }
