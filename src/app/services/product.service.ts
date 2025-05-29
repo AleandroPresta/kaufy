36605c2454bedfd84b5caad76412028414ab0973
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-29 18:33:55 +0200

    feat: integrate ngx-env for environment variable management
    - Added @ngx-env/builder to package.json for environment variable handling.
    - Refactored ProductService to use import.meta.env for API URL instead of environment.ts.
    - Removed environment.ts and environment.prod.ts files as they are no longer needed.
    - Created env.d.ts to define types for environment variables.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,15 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ProductService {
+    apiUrl = import.meta.env['NG_APP_API_URL'];
+
     constructor(private httpClient: HttpClient) {}
 
     getProducts(): Observable<Product[]> {
-        return this.httpClient.get<Product[]>(`${environment.apiUrl}/products`);
+        return this.httpClient.get<Product[]>(`${this.apiUrl}/products`);
     }
 }
