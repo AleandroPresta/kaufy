73d73a014c1373bdef0cf829734cb0ce84803cba
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-30 10:57:13 +0200

    added basic route transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { routeTransition } from '../route-transition';
 
 @Component({
     selector: 'app-root',
     imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
+    animations: [routeTransition],
 })
 export class AppComponent {
     title = 'kaufy';
+
+    constructor(protected route: ActivatedRoute) {}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import {
     provideClientHydration,
@@ -11,8 +11,9 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes, withViewTransitions()),
+        provideRouter(routes),
         provideClientHydration(withEventReplay()),
         provideHttpClient(),
+        provideAnimations(),
     ],
 };
