13cd24020745c07f9cbf01634edadeb35904f6c0
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-30 10:50:15 +0200

    feat: add route transition animations and update Angular dependencies

---
+++ src/route-transition.ts
@@ -0,0 +1,38 @@
+import {
+    query,
+    style,
+    trigger,
+    transition,
+    animate,
+} from '@angular/animations';
+
+export const routeTransition = trigger('routeTransition', [
+    transition('* => *', [
+        query(
+            ':enter',
+            [
+                style({
+                    opacity: 0,
+                    scale: 0.9,
+                }),
+            ],
+            { optional: true }
+        ),
+        query(
+            ':leave',
+            [
+                animate(
+                    '1s',
+                    style({
+                        opacity: 0,
+                        scale: 0.9,
+                    })
+                ),
+            ],
+            { optional: true }
+        ),
+        query(':enter', [animate('1s', style({ opacity: 1, scale: 1 }))], {
+            optional: true,
+        }),
+    ]),
+]);
