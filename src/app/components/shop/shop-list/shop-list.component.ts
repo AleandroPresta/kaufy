c6040e6a9ce5d851912272897b24e6e7b28f406f
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-06-01 17:03:39 +0200

    feat: enhance error handling in ShopListComponent and add noProductFound flag

--- src/app/components/shop/shop-list/shop-list.component.ts
+++ src/app/components/shop/shop-list/shop-list.component.ts
@@ -12,14 +12,19 @@ import { ProductService } from '../../../services/product.service';
 export class ShopListComponent {
     productList: Product[] = [];
     backendError: boolean = false; // This error appears when the backend is not running or connected properly
+    noProductFound: boolean = false; // This error appears when the backend is running but no products are found with the user query
     constructor(private productService: ProductService) {
         this.productService.getProducts().subscribe({
             next: products => {
                 this.productList = products;
             },
             error: error => {
-                console.error('Error fetching products:', error);
                 this.backendError = true;
+            },
+            complete: () => {
+                if (this.productList.length === 0) {
+                    this.noProductFound = true;
+                }
             },
         });
     }
