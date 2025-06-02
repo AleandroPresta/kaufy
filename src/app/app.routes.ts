fb396eb399f9e6c575d3e1d035ae094452afbd65
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-06-02 15:56:43 +0200

    feat: add product page route to shop component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { ShopComponent } from './components/shop/shop.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { CartComponent } from './components/cart/cart.component';
+import { ProductPageComponent } from './components/product/product-page/product-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/shop', pathMatch: 'full' },
     { path: 'shop', component: ShopComponent, data: { animation: 'ShopPage' } },
-    { path: 'profile', component: ProfileComponent, data: { animation: 'ProfilePage' } },
+    { path: 'shop/:slug', component: ProductPageComponent },
+    {
+        path: 'profile',
+        component: ProfileComponent,
+        data: { animation: 'ProfilePage' },
+    },
     { path: 'cart', component: CartComponent, data: { animation: 'CartPage' } },
 
     // Fallback route
