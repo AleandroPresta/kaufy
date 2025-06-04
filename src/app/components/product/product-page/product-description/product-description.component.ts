e75995daff7f5dde33985177d70f659b0fc4201c
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-06-04 17:35:30 +0200

    feat: enhance product description component with quantity input and cart button integration

--- src/app/components/product/product-page/product-description/product-description.component.ts
+++ src/app/components/product/product-page/product-description/product-description.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RatingComponent } from '../rating/rating.component';
+import { CartButtonComponent } from '../cart-button/cart-button.component';
 
 @Component({
     selector: 'app-product-description',
-    imports: [RatingComponent],
+    imports: [RatingComponent, CartButtonComponent],
     templateUrl: './product-description.component.html',
     styleUrl: './product-description.component.css',
 })
