3fe47cdf0d2a0a3f4cae0ce3831030cb1ee09991
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-06-05 14:46:50 +0200

    feat: enhance arrow-left button with routing capability and styling adjustments

--- src/app/components/product/product-page/arrow-left/arrow-left.component.ts
+++ src/app/components/product/product-page/arrow-left/arrow-left.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'arrow-left-button',
-    imports: [],
+    imports: [RouterLink],
     templateUrl: './arrow-left.component.html',
     styleUrl: './arrow-left.component.css',
 })
