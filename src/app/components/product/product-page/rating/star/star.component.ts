858d46703daefeac89ee0c4c5c4af6138b2cb4d6
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-06-05 14:40:52 +0200

    feat: implement dynamic star rating display with filled state binding

--- src/app/components/product/product-page/rating/star/star.component.ts
+++ src/app/components/product/product-page/rating/star/star.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-star',
-  imports: [],
-  templateUrl: './star.component.html',
-  styleUrl: './star.component.css'
+    selector: 'app-star',
+    imports: [],
+    templateUrl: './star.component.html',
+    styleUrl: './star.component.css',
 })
 export class StarComponent {
-
+    @Input() filled: boolean = false; // Indicates if the star is filled or not
 }
